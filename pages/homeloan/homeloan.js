1da1eaa7312b57c55fe6c7c0c98ee46a1c01bcbe
Leewen <3936044601@qq.com>
2020-07-10 14:36:52 +0800

    1、房贷分类

--- pages/homeloan/homeloan.js
+++ pages/homeloan/homeloan.js
@@ -5,8 +5,11 @@ const app = getApp()
 Page({
     data: {
         // 输入参数
+        current: 'tab1',
         price: 0.00,
+        price2: 0.00,
         rate: 4.90,
+        rate2: 3.25,
         index: 0,
         array: ['请选择贷款年限', '5年（60期）', '10年（120期）', '15年（180期）', '20年（240期）', '25年（300期）', '30年（360期）'],
         arrayValue: 0,
@@ -29,6 +32,21 @@ Page({
         minus: 0
     },
     onLoad: function () {},
+    handleChange ({ detail }) {
+        if (detail.key == 'tab2') {
+            this.setData({
+                rate: 3.25
+            })
+        } else {
+            this.setData({
+                rate: 4.90
+            })
+        }
+        this.setData({
+            current: detail.key,
+            calcClick: true
+        });
+    },
     bindInputPrice: function (e) {
         this.setData({
             price: e.detail.value
@@ -39,10 +57,21 @@ Page({
             rate: e.detail.value
         })
     },
+    bindInputPrice2: function (e) {
+        this.setData({
+            price2: e.detail.value
+        })
+    },
+    bindInputRate2: function (e) {
+        this.setData({
+            rate2: e.detail.value
+        })
+    },
     changeNum: function (e) {
         this.setData({
             value: e.detail.value
         })
+        this.bindTapCalc()
     },
     bindPickerChange: function (e) {
         const value = e.detail.value
@@ -92,51 +121,104 @@ Page({
             })
             return
         }
-        if (this.data.value == 1) {
-            this.calc()
+        if (this.data.current == "tab3" && this.data.rate == "" && this.data.rate2 == "") {
+            wx.showToast({
+                title: '请输入贷款利率',
+                icon: 'none'
+            })
+            return
+        }
+
+        if (this.data.current == 'tab3') {
+            if (this.data.value == 1) {
+                const map = this.calc(this.data.rate, this.data.price)
+                const map2 = this.calc(this.data.rate2, this.data.price2)
+                let monthPay = parseFloat(map.get("monthPay")) + parseFloat(map2.get("monthPay"))
+                let loanMoney = parseFloat(map.get("loanMoney")) + parseFloat(map2.get("loanMoney"))
+                let interestPay = parseFloat(map.get("interestPay")) + parseFloat(map2.get("interestPay"))
+                let totalPay = parseFloat(map.get("totalPay")) + parseFloat(map2.get("totalPay"))
+                this.setData({
+                    loanMoney: loanMoney.toFixed(2),
+                    monthPay: monthPay.toFixed(2),
+                    interestPay: interestPay.toFixed(2),
+                    totalPay: totalPay.toFixed(2)
+                })
+            } else {
+                const map = this.calc2(this.data.rate, this.data.price)
+                const map2 = this.calc2(this.data.rate2, this.data.price2)
+                let minus = parseFloat(map.get("minus")) + parseFloat(map2.get("minus"))
+                let loanMoney = parseFloat(map.get("loanMoney")) + parseFloat(map2.get("loanMoney"))
+                let monthPay = parseFloat(map.get("monthPay")) + parseFloat(map2.get("monthPay"))
+                let interestPay = parseFloat(map.get("interestPay")) + parseFloat(map2.get("interestPay"))
+                let totalPay = parseFloat(map.get("totalPay")) + parseFloat(map2.get("totalPay"))
+                this.setData({
+                    minus: minus.toFixed(2),
+                    loanMoney: loanMoney.toFixed(2),
+                    monthPay: monthPay.toFixed(2),
+                    interestPay: interestPay.toFixed(2),
+                    totalPay: totalPay.toFixed(2)
+                })
+            }
         } else {
-            this.calc2()
+            if (this.data.value == 1) {
+                const map = this.calc(this.data.rate, this.data.price)
+                this.setData({
+                    loanMoney: map.get("loanMoney").toFixed(2),
+                    monthPay: map.get("monthPay").toFixed(2),
+                    interestPay: map.get("interestPay").toFixed(2),
+                    totalPay: map.get("totalPay").toFixed(2)
+                })
+            } else {
+                const map = this.calc2(this.data.rate, this.data.price)
+                this.setData({
+                    minus: map.get("minus").toFixed(2),
+                    loanMoney: map.get("loanMoney").toFixed(2),
+                    monthPay: map.get("monthPay").toFixed(2),
+                    interestPay: map.get("interestPay").toFixed(2),
+                    totalPay: map.get("totalPay").toFixed(2)
+                })
+            }
         }
         this.setData({
             calcClick: false
         })
     },
     // 计算
-    calc: function () {
+    calc: function (rate, price) {
         // 等额本息公式：月还款额=[贷款本金*月利率*(1+月利率)^还款月数]÷[(1+月利率)^还款月数-1]
-        let monthRate = parseFloat(this.data.rate / 12 / 100)
-        let loanMoney = parseFloat(this.data.price)
+        let monthRate = parseFloat(rate / 12 / 100)
+        let loanMoney = parseFloat(price)
         let x = parseFloat(1.00 + monthRate)
         let pow = Math.pow(x, this.data.arrayValue)
-        let monthPay = parseFloat((loanMoney * monthRate * pow) / (pow - 1)).toFixed(2)
-        let interestPay = parseFloat(this.data.arrayValue * monthPay - loanMoney).toFixed(2)
-        let totalPay = parseFloat(this.data.arrayValue * monthPay).toFixed(2)
-        this.setData({
-            loanMoney: loanMoney,
-            monthPay: monthPay,
-            interestPay: interestPay,
-            totalPay: totalPay
-        })
+        let monthPay = parseFloat((loanMoney * monthRate * pow) / (pow - 1))
+        let interestPay = parseFloat(this.data.arrayValue * monthPay - loanMoney)
+        let totalPay = parseFloat(this.data.arrayValue * monthPay)
+        let map = new Map()
+        map.set("loanMoney", loanMoney)
+        map.set("monthPay", monthPay)
+        map.set("interestPay", interestPay)
+        map.set("totalPay", totalPay)
+        return map
     },
-    calc2: function () {
+    calc2: function (rate, price) {
         // 1.每月月供额=(贷款本金÷还款月数)+(贷款本金-已归还本金累计额)×月利率
         // 2.每月应还本金=贷款本金÷还款月数
         // 3.每月应还利息=剩余本金×月利率=(贷款本金-已归还本金累计额)×月利率
         // 4.每月月供递减额=每月应还本金×月利率=贷款本金÷还款月数×月利率
         // 5.总利息=〔(总贷版款额÷权还款月数+总贷款额×月利率)+总贷款额÷还款月数×(1+月利率)〕÷2×还款月数-总贷款额
-        let monthRate = parseFloat(this.data.rate / 12 / 100)
-        let loanMoney = parseFloat(this.data.price)
+        let monthRate = parseFloat(rate / 12 / 100)
+        let loanMoney = parseFloat(price)
         let x = parseFloat(1.00 + monthRate)
-        let monthPay = parseFloat((loanMoney / this.data.arrayValue) + (loanMoney * monthRate)).toFixed(2)
-        let minus = parseFloat((loanMoney / this.data.arrayValue) * monthRate).toFixed(2)
+        let monthPay = parseFloat((loanMoney / this.data.arrayValue) + (loanMoney * monthRate))
+        let minus = parseFloat((loanMoney / this.data.arrayValue) * monthRate)
         let interestPay = parseFloat(((loanMoney / this.data.arrayValue + loanMoney * monthRate) + loanMoney / this.data.arrayValue * x) / 2 * this.data.arrayValue - loanMoney)
-        let totalPay = parseFloat(loanMoney + interestPay).toFixed(2)
-        this.setData({
-            minus: minus,
-            loanMoney: loanMoney,
-            monthPay: monthPay,
-            interestPay: interestPay.toFixed(2),
-            totalPay: totalPay
-        })
+        let totalPay = parseFloat(loanMoney + interestPay)
+        let map = new Map()
+        map.set("minus", minus)
+        map.set("loanMoney", loanMoney)
+        map.set("monthPay", monthPay)
+        map.set("interestPay", interestPay)
+        map.set("totalPay", totalPay)
+        return map
     }
 })
